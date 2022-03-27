0fd2ab7b23820d5ca050f7b9b7ab9de9dc47031f
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-27 20:22:59 +0530

    Welcome To Generics Problems

---
+++ FindMaxUsingGenerics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the generic problems");
+            MaxNumber maxNumber = new MaxNumber();
+            Console.WriteLine("Maximun Number Is :" + maxNumber.Maximum(10, 20, 15));
+            Console.ReadKey();
+        }
+    }
+}
+
+       
+
